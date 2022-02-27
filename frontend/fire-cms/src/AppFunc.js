ac49c18b8fef93280465c46dbd9d98e40fd5aacc
yusufatalay <yusuftahaatalay14@gmail.com>
2022-02-27 22:32:50 +0300

    Implement outline of the main page in front-end part.

---
+++ frontend/fire-cms/src/AppFunc.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import CoursesFunc from './components/CoursesFunc'
+import OneCourseFunc from './components/OneCourseFunc'
+function AppFunc(props) {
+  // put some authentication checks in here later.
+
+  return (
+    <Router>
+      <div className='container'>
+        <div className='row'>
+          <div className='col mt-3'>
+            <h1 className='mt-3'>
+              FireCMS
+            </h1>
+          </div>
+          <hr className='mb-3'></hr>
+        </div>
+
+        <div className='row'>
+          <div className='col-md-2'>
+            <nav>
+              <ul className='list-group'>
+                <li className='list-group-item'>
+                  <Link to="/">Home</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="/courses">Courses</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="/gigsites">Gig Sites</Link>
+                </li>
+                <li className='list-group-item'>
+                  <Link to="/jobs">Jobs</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className='col-md-10'>
+            <Switch>
+              <Route path="/course/:courseName">
+                <OneCourseFunc />
+              </Route>
+              <Route path="/courses">
+                <CoursesFunc />
+              </Route>
+
+
+              {/* <Route path="/">
+                <HomeFunc />
+              </Route> */}
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+
+export default AppFunc
---
+++ frontend/fire-cms/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AppFunc from './AppFunc.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppFunc />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
